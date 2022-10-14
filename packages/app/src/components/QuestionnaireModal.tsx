ed7e6eb95ce734bc366d012ad76c610d3bc83c6f
atsuki-t <atsuki-t@weseek.co.jp>
2022-10-14 12:19:22 +0000

    i18n questionnaire modal

--- packages/app/src/components/QuestionnaireModal.tsx
+++ packages/app/src/components/QuestionnaireModal.tsx
@@ -27,12 +27,12 @@ const QuestionnaireCompletionModal = (props: ModalProps): JSX.Element => {
       <ModalBody className="bg-primary overflow-hidden p-0" style={{ borderRadius: 8 }}>
         <div className="bg-white m-2 p-4" style={{ borderRadius: 8 }}>
           <div className="text-center">
-            <h2 className="my-4">GROWI サービス改善のためのアンケート</h2>
-            <p className="mb-1">アンケートの送信が完了しました。</p>
-            <p>アンケートのご回答誠にありがとうございました。</p>
+            <h2 className="my-4">{t('questionnaire.title')}</h2>
+            <p className="mb-1">{t('questionnaire.successfully_submit')}</p>
+            <p>{t('questionnaire.thanks_for_answer')}</p>
           </div>
           <div className="text-center my-3">
-            <span style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={onClose}>閉じる</span>
+            <span style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={onClose}>{t('Close')}</span>
           </div>
         </div>
       </ModalBody>
@@ -72,7 +72,7 @@ const QuestionnaireModal = (props: ModalProps): JSX.Element => {
       growiVersion,
     };
 
-    // TODO: アンケート結果を送信する
+    // TODO: send qestionnaire data
 
     onClose();
     setQuestionnaireCompletionModal(true);
@@ -89,15 +89,17 @@ const QuestionnaireModal = (props: ModalProps): JSX.Element => {
         <ModalBody className="bg-primary overflow-hidden p-0" style={{ borderRadius: 8 }}>
           <div className="bg-white m-2 p-4" style={{ borderRadius: 8 }}>
             <div className="text-center">
-              <h2 className="my-4">GROWI サービス改善のためのアンケート</h2>
-              <p className="mb-1">GROWI をご利用の皆さまに更にご満足いただけるよう</p>
-              <p>皆さまからのご意見を参考にサービス改善に務めてまいります。</p>
+              <h2 className="my-4">{t('questionnaire.title')}</h2>
+              <p className="mb-1">{t('questionnaire.more_satisfied_services')}</p>
+              <p>{t('questionnaire.strive_to_improve_services')}</p>
             </div>
             <form className="px-5" onSubmit={submitHandler}>
               <div className="form-group row mt-5">
-                <label className="col-sm-5 col-form-label" htmlFor="satisfaction"><span className="badge badge-primary mr-2">必須</span>GROWI の満足度</label>
+                <label className="col-sm-5 col-form-label" htmlFor="satisfaction">
+                  <span className="badge badge-primary mr-2">{t('Required')}</span>{t('questionnaire.satisfaction_with_growi')}
+                </label>
                 <select className="col-sm-7 form-control" name="satisfaction" id="satisfaction" required>
-                  <option value="">▼ 選択してください</option>
+                  <option value="">▼ {t('Select')}</option>
                   <option>1</option>
                   <option>2</option>
                   <option>3</option>
@@ -106,38 +108,38 @@ const QuestionnaireModal = (props: ModalProps): JSX.Element => {
                 </select>
               </div>
               <div className="form-group row mt-3">
-                <label className="col-sm-5 col-form-label" htmlFor="lengthOfExperience">GROWI の利用歴</label>
+                <label className="col-sm-5 col-form-label" htmlFor="lengthOfExperience">{t('questionnaire.history_of_growi_use')}</label>
                 <select
                   name="lengthOfExperience"
                   id="lengthOfExperience"
                   className="col-sm-7 form-control"
                 >
-                  <option value="">▼ 選択してください</option>
-                  <option>2年以上</option>
-                  <option>1年以上2年未満</option>
-                  <option>6ヶ月以上1年未満</option>
-                  <option>3ヶ月以上6ヶ月未満</option>
-                  <option>1ヶ月以上3ヶ月未満</option>
-                  <option>1ヶ月未満</option>
+                  <option value="">▼ {t('Select')}</option>
+                  <option>{t('questionnaire.length_of_experience.more_than_two_years')}</option>
+                  <option>{t('questionnaire.length_of_experience.one_to_two_years')}</option>
+                  <option>{t('questionnaire.length_of_experience.six_months_to_one_year')}</option>
+                  <option>{t('questionnaire.length_of_experience.three_months_to_six_months')}</option>
+                  <option>{t('questionnaire.length_of_experience.one_month_to_three_months')}</option>
+                  <option>{t('questionnaire.length_of_experience.less_than_one_month')}</option>
                 </select>
               </div>
               <div className="form-group row mt-3">
-                <label className="col-sm-5 col-form-label" htmlFor="position">職種</label>
+                <label className="col-sm-5 col-form-label" htmlFor="position">{t('questionnaire.position')}</label>
                 <input className="col-sm-7 form-control" type="text" name="position" id="position" />
               </div>
               <div className="form-group row mt-3">
-                <label className="col-sm-5 col-form-label" htmlFor="occupation">役職</label>
+                <label className="col-sm-5 col-form-label" htmlFor="occupation">{t('questionnaire.occupation')}</label>
                 <input className="col-sm-7 form-control" type="text" name="occupation" id="occupation" />
               </div>
               <div className="form-group row mt-3">
-                <label className="col-sm-5 col-form-label" htmlFor="commentText">GROWI へのコメント</label>
+                <label className="col-sm-5 col-form-label" htmlFor="commentText">{t('questionnaire.comment_on_growi')}</label>
                 <textarea className="col-sm-7 form-control" name="commentText" id="commentText" rows={5} />
               </div>
               <div className="text-center mt-5">
-                <button type="submit" className="btn btn-primary">回答する</button>
+                <button type="submit" className="btn btn-primary">{t('questionnaire.answer')}</button>
               </div>
               <div className="text-center my-3">
-                <span style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={onClose}>閉じる</span>
+                <span style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={onClose}>{t('Close')}</span>
               </div>
             </form>
           </div>
